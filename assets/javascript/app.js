92b4776afe83778140a29b778c3a0cb607c00d49
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-03-16 11:00:58 -0700

    Adding news API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,12 @@
 
 
- // NY TIMES queryURL for API
- var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+$(document).ready(function() {
 
+// NY TIMES queryURL for API
+ var newsURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+ var newsParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
  
+ console.log("hello");
+
+})
+
