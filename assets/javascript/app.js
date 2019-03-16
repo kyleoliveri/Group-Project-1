b745d8e0c0a5090ca881737f77ad79fe56a0b80b
Jenn <jenn@Jenns-MacBook-Pro.local>
2019-03-16 11:14:13 -0700

    weather API start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 
 
- // NY TIMES queryURL for API
- var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+// weather queryURL
+// var weatherCity = $("#search-city").val();
+// var weatherURL = "api.openweathermap.org/data/2.5/forecast?q={city name},{country code}"
 
- 
+//  NY TIMES queryURL for API
+//  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+$(h1).append("testing jquery");
